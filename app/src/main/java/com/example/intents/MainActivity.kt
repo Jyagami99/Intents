9b8d4218acf21fda14043aaa75e62fc09badacb4
João Marcelo Simão de Castro <jm.castro@usp.br>
2025-05-06 01:23:39 -0300

    feat: add ActivityMainBinding

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.intents
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var parameterArl: ActivityResultLauncher<Intent>
+    private lateinit var cppArl: ActivityResultLauncher<String>
+    private lateinit var pickImageArl: ActivityResultLauncher<Intent>
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
