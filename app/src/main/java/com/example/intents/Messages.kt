50d09716299419f0dbd002c5338418f8cf9c2ef8
João Marcelo Simão de Castro <jm.castro@usp.br>
2025-05-06 03:01:29 -0300

    feat: implements Messages class

---
+++ app/src/main/java/com/example/intents/Messages.kt
@@ -0,0 +1,7 @@
+package com.example.intents
+
+object Messages {
+    const val CALL_ERROR = "Permissão necessária para ligar para este número"
+    const val OPEN_MESSAGE = "Você clicou no open"
+    const val CHOOSE_BROWSER = "Escolha seu navegador favorito"
+}
